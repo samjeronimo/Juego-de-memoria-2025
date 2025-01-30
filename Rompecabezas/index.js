9de39e57574cccf50cffd454026c176a8d579d51
samjeronimo <159475058+samjeronimo@users.noreply.github.com>
2025-01-30 13:43:17 -0600

    Add files via upload

---
+++ Rompecabezas/index.js
@@ -0,0 +1,23 @@
+let DOM = document.querySelector('#root');
+
+let contenedor = document.createElement('div');
+contenedor.className = "contenedor";
+
+let header = document.createElement('div');
+header.className = "div-header";
+contenedor.appendChild(header);
+
+let progreso = document.createElement('div');
+progreso.className = "div-progreso";
+contenedor.appendChild(progreso);
+
+let tablero = document.createElement('div');
+tablero.className = "div-tablero"
+contenedor.appendChild(tablero);
+
+let footer = document.createElement('div');
+footer.className = "div-footer";
+contenedor.appendChild(footer);
+
+
+DOM.appendChild(contenedor);
